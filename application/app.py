995f87e8aea671c55e97f3161e8e3e74d0e957d6
Gabriel C3n <ur.gabriel@outlook.com>
2020-06-15 21:47:11 +0200

    Index date-time utc

--- application/app.py
+++ application/app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
 from flask import request, make_response
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__) 
 Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This doc carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    user_agent = request.headers.get('User-Agent')
-    # return '<h1>Your browser is %s!</h1>' % user_agent
-    return response
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
