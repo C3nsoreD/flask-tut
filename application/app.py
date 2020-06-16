315dc5abef85f0c56aa5aaffff4acf50f4ca4bc7
Gabriel C3n <ur.gabriel@outlook.com>
2020-06-16 22:54:10 +0200

    Forms

--- application/app.py
+++ application/app.py
@@ -2,15 +2,26 @@ from flask import Flask, render_template
 from flask import request, make_response
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField 
+from wtforms.validators import Required
 from datetime import datetime
 
+
 app = Flask(__name__) 
+app.config['SECRET_KEY'] = 'c3n_code'
 Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if  form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -24,6 +35,12 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('error/500.html'), 500
 
+#Froms ----------------------->
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField("Submit")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+Form
