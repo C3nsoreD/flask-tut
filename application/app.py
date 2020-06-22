a937fb202e3580698ed328e5630291502a545086
Gabriel C3n <ur.gabriel@outlook.com>
2020-06-22 18:55:29 +0200

    database integration

--- application/app.py
+++ application/app.py
@@ -19,11 +19,11 @@ app.config['SECRET_KEY'] = 'c3n_code'
 Bootstrap(app)
 moment = Moment(app)
 
-
-basebir = os.path.abspath(os.path.dirname(__name__))
-app.config['SQLALCHEMY_DATABSE_URI'] =  'sqlite:///'+os.path.join(basebir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
+basebir = os.path.abspath(os.path.dirname(__name__))
+app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///'+os.path.join(basebir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,15 +31,24 @@ def index():
     # name = None
     form = NameForm()
     if  form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        # check if the user is known
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+            
         session['name'] = form.name.data
         form.name.data = ''
-        # name = form.name.data
+
         return redirect(url_for('index'))
 
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', 
+        current_time=datetime.utcnow(), 
+        form=form, name=session.get('name'),
+        known = session.get('known' , False))
 
 @app.route('/user/<name>')
 def user(name):
@@ -65,7 +74,6 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    
     users = db.relationship('User', backref='role')
 
     def __repr__(self):
@@ -76,12 +84,11 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
     def __repr__(self):
         return f'<User {self.username}'
 
     
 if __name__ == "__main__":
     app.run(debug=True)
-Form
