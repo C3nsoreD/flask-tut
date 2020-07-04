90941e951c663b495615e0b932730f75a89ef2df
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-04 15:50:06 +0200

    Working app structure

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
 
-basebir = os.path.abspath(os.path.dirname(__name__))
+basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLACHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     BLOG_TWO_MAIL_SUBJECT_PREFIX = '[BLOG 2.0]'
     BLOG_TWO_MAIL_SENDER = 'Blog 2 Admin <blogtwo@example.com'
     BLOG_TWO_ADMIN = os.environ.get("BLOG_TWO_ADMIN")
@@ -24,7 +25,7 @@ class DevelopmentConfig(Config):
     MAIL_PASSWORD =  os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
-class TestingConfg(config):
+class TestingConfg(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
--- manage.py
+++ manage.py
@@ -6,12 +6,21 @@ from flask.cli import AppGroup
 
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') of 'default')
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
-    
+@app.cli.command()
+# @click.argument('test_names', nargs=-1)
+def test():
+    """Run the unit tests."""
+
+    import unittest
+
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
