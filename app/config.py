b11e2343cb84c8ff7e32108290d60c4f194d2cc1
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-04 12:29:27 +0200

    App structure

---
+++ app/config.py
@@ -0,0 +1,42 @@
+import os
+
+
+basebir = os.path.abspath(os.path.dirname(__name__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLACHEMY_COMMIT_ON_TEARDOWN = True
+    BLOG_TWO_MAIL_SUBJECT_PREFIX = '[BLOG 2.0]'
+    BLOG_TWO_MAIL_SENDER = 'Blog 2 Admin <blogtwo@example.com'
+    BLOG_TWO_ADMIN = os.environ.get("BLOG_TWO_ADMIN")
+
+    @staticmethod
+    def init_app(app):
+        # Specific initializations can be performed.
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'stmp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =  os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD =  os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfg(config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfg,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
--- application/app.py
+++ application/app.py
@@ -127,25 +127,6 @@ class NameForm(Form):
     submit = SubmitField("Submit")
 
 
-### Models ---------------------->
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return f'<User {self.name}>'
-    
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    
-    def __repr__(self):
-        return f'<User {self.username}>'
-
 
 if __name__ == "__main__":
     db.create_all()
