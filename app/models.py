ffef1ab8f6d34abf29ab68a108e89447b70248c4
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-06 20:29:20 +0200

    Documentaion and auth blueprint

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 ### Models ---------------------->
 class Role(db.Model):
@@ -12,9 +13,10 @@ class Role(db.Model):
     def __repr__(self):
         return f'<User {self.name}>'
     
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
@@ -24,13 +26,27 @@ class User(db.Model):
 
     @property
     def password(self):
+        '''
+        Password property to prevent reading passwords directly
+        '''
         raise AttributeError('Password is not a readable attribute')
     
     @password.setter
     def password(self, password):
+        '''
+        Password setter. Applies hashing to the password
+        '''
         self.password_hash = generate_password_hash(password)
 
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    '''
+    This sets the callback for reloading a user from the session.
+    '''
+    return User.query.get(int(user_id))
 
