3e489109c05eea6b62a8e00d48bd477969687d37
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-13 08:26:27 +0200

    Errors in main/forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,4 +12,4 @@ class EditProfileForm(Form):
     name = StringField('Real name', validators=[Length(0, 64)])
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextField('About me')
-    submit = StringField('Submit')
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 
 from . import main
 from .forms import NameForm, EditProfileForm
@@ -63,7 +63,7 @@ def for_moderators_only():
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form EditProfileForm()
+    form = EditProfileForm()
     if form.validate_on_submit():
         current_user.name = form.name.data
         current_user.location = form.location.data
@@ -75,4 +75,4 @@ def edit_profile():
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', form)
+    return render_template('edit_profile.html', form=form)
