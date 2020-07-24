99540389584096feef32e6fc6da1dd501ea9cd31
Gabriel C3n <ur.gabriel@outlook.com>
2020-07-24 12:25:12 +0200

    feature: show followers posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, flash, request, current_app
-
+from flask import (
+    render_template, session, redirect, url_for, flash, request, current_app, make_response
+)
 from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, BlogPostForm
 from .. import db
@@ -20,15 +21,39 @@ def index():
         return redirect(url_for('main.index'))
     # posts = Post.query.order_by(Post.timestamp.desc()).all()
         # return redirect(url_for('.index')) # same as: redirect(url_for('main.index'))
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
         error_out=False
     )
     posts = pagination.items
     return render_template('index.html',
         current_time=datetime.utcnow(),
-        form=form, posts=posts, pagination=pagination)
+        form=form, posts=posts, show_followed=show_followed, pagination=pagination)
+
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
+
 
 # User profile link
 @main.route('/user/<username>')
